9d53e998d7a91997d0bdb1273e30bfe8bc1a134d
koomited <koomi@aims.ac.za>
2025-05-14 10:09:30 +0200

    exceptions, logger and utils added

--- setup.py
+++ setup.py
@@ -1,55 +1,29 @@
-import os
-from pathlib import Path
-import logging
+import setuptools
 
-logging.basicConfig(filename='running_info.log', level=logging.INFO, format='[%(asctime)s]: %(message)s:')
-
-
-project_name = "mlProject"
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 
-list_of_files = [
-    ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/entity/config_entity.py",
-    f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
-    "params.yaml",
-    "schema.yaml",
-    "main.py",
-    "app.py",
-    "Dockerfile",
-    "requirements.txt",
-    "setup.py",
-    "research/trials.ipynb",
-    "templates/index.html",
-    "test.py"
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-ML-Project-with-MLflow"
+AUTHOR_USER_NAME = "Toussinet"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "koomi@aims.ac.za"
 
 
-]
-
-
-for filepath in list_of_files:
-    filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
-
-
-    if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file: {filename}")
-
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
-            pass
-            logging.info(f"Creating empty file: {filepath}")
-
-
-    else:
-        logging.info(f"{filename} already exists")
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
